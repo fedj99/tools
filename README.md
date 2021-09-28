56431463f33080b3eff71736e8b2b3e1bfac302f
Federico Mantovani <fettemanto@gmail.com>
2021-09-28 22:07:58 +0200

    Added set-llvm-version script and documentation

--- README.md
+++ README.md
@@ -54,6 +54,26 @@ butcher.sh has the following dependencies:
 
 However, you do not need to install them manually, as the script will already do this for you automatically (simply answer `y` when prompted or `--install-deps` in CLI mode).
 
+## set-llvm-version.sh
+
+Changing defaults versions for llvm programs is tedious and time-consuming. This little script aims to make the process lighning-fast, by searching for all installed llvm programs and configuring them using `update-alternatives` (so make sure that is installed)
+
+### Usage
+
+As usual, before first use, you should execute `chmod +x ./set-llvm-version.sh` to make it executable.
+
+You can set the default version of all llvm programs (including clang, llc etc.) using the following command:
+
+```bash
+./set-llvm-version.sh --all XX
+```
+
+where `XX` is the version number you want to set.
+
+To set just a single program in the llvm family, use the `--name <prog> <ver>` option.
+
+The script will try to automatically figure out where llvm is installed. If it fails to do so, you can provide a custom installation directory with the `--install-dir` option.
+
 ## random.sh
 
 Simple script to generate random md5 hashes of arbitrary length. Simply download the script and make it executable with `chmod +x random.sh`.
---
+++ set-llvm-version.sh
@@ -0,0 +1,102 @@
+set -e
+
+APP_NAME='set-llvm-version'
+APP_VERSION='1.0.0'
+
+function usage {
+	echo "$APP_NAME - version $APP_VERSION"
+	echo ""
+	echo "SYNOPSIS"
+	echo "	Sets the default version of any program included in the llvm package."
+	echo ""
+	echo "USAGE"
+	echo "	./set-llvm-version.sh [...options]"
+	echo ""
+	echo "OPTIONS"
+	echo "	-n, --name <prog> <ver>  Sets the default version for the specified program"
+	echo "	-a, --all <ver>          Sets the default version on ALL LLVM-related programs found in"
+	echo "	                         LLVM's installation directory"
+	echo "	-i, --install-dir <dri>  Optionally specify a non-standard installation directory"
+	echo "	-h, --help               Display this help message"
+	echo ""
+	echo "	<prog>                   Program name to set version of (e.g. 'llc')"
+	echo "	<ver>                    Desired version (e.g. '9')"
+	echo "	<dir>                    Non-standard installation directory (e.g. '/usr/local/bin')"
+	echo ""
+	echo "AUTHOR"
+	echo "	Federico Mantovani <fmantova@student.ethz.ch>"
+	echo ""
+	echo "LICENSE"
+	echo "	MIT License, Copyright (c) 2021 Federico Mantovani"
+}
+
+function check_install_dir {
+	if [ -z $installdir ]; then
+		echo "Unable to locate installation directory - Please provide one with --install-dir (see --help for more info)"
+		exit 1
+	fi
+}
+
+function check_ver {
+	if ! [[ $ver =~ [0-9]+ ]]; then
+		echo "Version must be an integer"
+		exit 1
+	fi
+}
+
+function config {
+	check_ver
+	check_install_dir
+
+	sudo update-alternatives --install "$installdir/$1" "$1" "$installdir/$1-$ver" $ver
+	sudo update-alternatives --config "$1"
+}
+
+function all {
+	echo "Setting llvm version to $ver..."
+	progs=$(ls $installdir | grep "\(llvm\|llc\|clang\).*-$ver")
+	for prog in $progs; do
+		config "${prog%-$ver}"
+	done
+}
+
+# Argument parsing
+program="main"
+args=()
+
+custom_install_dir="" # Only set if using --install-dir
+progname=""           # Only set if using --name
+ver=""                # Set in all cases
+
+while [ "$#" -gt 0 ]; do
+	case "$1" in
+	-h | --help)
+		program="usage"
+		shift 1
+		;;
+	-n | --name)
+		progname=$2
+		ver=$3
+		program="config $2"
+		shift 3
+		;;
+	-a | --all)
+		program="all"
+		ver=$2
+		shift 2
+		;;
+	-i | --install-dir)
+		custom_install_dir=$2
+		shift 2
+		;;
+	*)
+		args+=("$1")
+		shift 1
+		;;
+	esac
+done
+
+# Try to automatically set installation directory
+installdir=$(dirname $(which llc) || echo '')
+
+eval $program
